b4d053922f9319f92698948f450015c641ec071f
ZawHein337 <www.zawhein337@gmail.com>
2026-06-20 16:09:43 +0630

    Update images.dart

--- lib/util/images.dart
+++ lib/util/images.dart
@@ -8,6 +8,7 @@ class Images {
   static const String call = 'assets/image/call.png';
   static const String checked = 'assets/image/checked.png';
   static const String english = 'assets/image/english.png';
+static const String myanmar = 'assets/images/myanmar.png';
   static const String locationMarker = 'assets/image/location_marker.png';
   static const String lock = 'assets/image/lock.png';
   static const String mail = 'assets/image/mail.png';
